c453870e75c99ed0257d03f1886e16faa92c3458
lakdav <ladav2018@gmail.com>
2023-03-26 03:00:03 +0300

    feat:add friends section for pets page

--- shelter/js/data.js
+++ shelter/js/data.js
@@ -1,4 +1,4 @@
-export default data = [
+export default [
 	{
 		_id: 1,
 		name: 'Jennifer',
@@ -6,6 +6,7 @@ export default data = [
 		breed: 'Labrador',
 		info:
 			"Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won't hesitate to play up a storm in the house if she has all of her favorite toys.",
+		img: 'pets-jennifer-min.png',
 	},
 	{
 		_id: 2,
@@ -14,6 +15,7 @@ export default data = [
 		breed: 'Shih tzu',
 		info:
 			"Sophia here and I'm looking for my forever home to live out the best years of my life. I am full of energy. Everyday I'm learning new things, like how to walk on a leash, go potty outside, bark and play with toys and I still need some practice.",
+		img: 'pets-katrine-1-min.png',
 	},
 	{
 		_id: 3,
@@ -22,6 +24,7 @@ export default data = [
 		breed: 'Golden Retriever',
 		info:
 			'Woody is a handsome 3 1/2 year old boy. Woody does know basic commands and is a smart pup. Since he is on the stronger side, he will learn a lot from your training. Woody will be happier when he finds a new family that can spend a lot of time with him.',
+		img: 'pets-woody-min.png',
 	},
 	{
 		_id: 4,
@@ -30,6 +33,7 @@ export default data = [
 		breed: 'Jack Russell Terrier',
 		info:
 			'Scarlett is a happy, playful girl who will make you laugh and smile. She forms a bond quickly and will make a loyal companion and a wonderful family dog or a good companion for a single individual too since she likes to hang out and be with her human.',
+		img: 'pets-scarlet-min.png',
 	},
 	{
 		_id: 5,
@@ -38,6 +42,7 @@ export default data = [
 		breed: 'British Shorthair',
 		info:
 			'Katrine is a beautiful girl. She is as soft as the finest velvet with a thick lush fur. Will love you until the last breath she takes as long as you are the one. She is picky about her affection. She loves cuddles and to stretch into your hands for a deeper relaxations.',
+		img: 'pets-katrine-2-min.png',
 	},
 	{
 		_id: 6,
@@ -46,6 +51,7 @@ export default data = [
 		breed: 'British Shorthair',
 		info:
 			'Timmy is an adorable grey british shorthair male. He loves to play and snuggle. He is neutered and up to date on age appropriate vaccinations. He can be chatty and enjoys being held. Timmy has a lot to say and wants a person to share his thoughts with.',
+		img: 'pets-timmy-min.png',
 	},
 	{
 		_id: 7,
@@ -54,6 +60,7 @@ export default data = [
 		breed: 'British Shorthair',
 		info:
 			'Freddie is a little shy at first, but very sweet when he warms up. He likes playing with shoe strings and bottle caps. He is quick to learn the rhythms of his human’s daily life. Freddie has bounced around a lot in his life, and is looking to find his forever home.',
+		img: 'pets-katrine-min.png',
 	},
 	{
 		_id: 8,
@@ -62,5 +69,6 @@ export default data = [
 		breed: 'Jack Russell Terrier',
 		info:
 			'This cute boy, Charly, is three years old and he likes adults and kids. He isn’t fond of many other dogs, so he might do best in a single dog home. Charly has lots of energy, and loves to run and play. We think a fenced yard would make him very happy.',
+		img: 'pets-charly-min.png',
 	},
 ];
--- shelter/js/index.js
+++ shelter/js/index.js
@@ -1,3 +1,5 @@
+import data from './data.js';
+console.log(data);
 //navigation contro; for mobile
 const navigationMenu = document.getElementById('navigation');
 const navigationToggleBtn = document.getElementById('toggle-nav');
@@ -9,3 +11,18 @@ navigationToggleBtn.addEventListener('click', () => {
 	);
 	navigationMenu.classList.toggle('open');
 });
+
+const pets_friends_layout = document.getElementById('pets_layout');
+if (pets_friends_layout) {
+	let HTML_STRING = '';
+	for (let i = 0; i < data.length; i++) {
+		HTML_STRING += `<article class="card">
+   <div class="card__image">
+	   <img src="./assets/pets/${data[i].img}" alt="${data[i].name}">
+   </div>
+   <h2 class="card__name">${data[i].name}</h2>
+   <button id="card-modal" class="card__btn btn outlined">Learn more</button>
+   </article>`;
+	}
+	pets_friends_layout.insertAdjacentHTML('afterbegin', HTML_STRING);
+}
