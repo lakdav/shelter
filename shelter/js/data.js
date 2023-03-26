76940c9d39d773716fb5990b77f0f73c1e54737f
lakdav <ladav2018@gmail.com>
2023-03-26 14:15:46 +0300

    feat:add pagination

--- shelter/js/data.js
+++ shelter/js/data.js
@@ -1,6 +1,15 @@
 export default [
 	{
 		_id: 1,
+		name: 'Katrine',
+		animal: 'cat',
+		breed: 'British Shorthair',
+		info:
+			'Katrine is a beautiful girl. She is as soft as the finest velvet with a thick lush fur. Will love you until the last breath she takes as long as you are the one. She is picky about her affection. She loves cuddles and to stretch into your hands for a deeper relaxations.',
+		img: 'pets-katrine-2-min.png',
+	},
+	{
+		_id: 2,
 		name: 'Jennifer',
 		animal: 'dog',
 		breed: 'Labrador',
@@ -8,15 +17,7 @@ export default [
 			"Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won't hesitate to play up a storm in the house if she has all of her favorite toys.",
 		img: 'pets-jennifer-min.png',
 	},
-	{
-		_id: 2,
-		name: 'Sophia',
-		animal: 'dog',
-		breed: 'Shih tzu',
-		info:
-			"Sophia here and I'm looking for my forever home to live out the best years of my life. I am full of energy. Everyday I'm learning new things, like how to walk on a leash, go potty outside, bark and play with toys and I still need some practice.",
-		img: 'pets-katrine-1-min.png',
-	},
+
 	{
 		_id: 3,
 		name: 'Woody',
@@ -28,24 +29,15 @@ export default [
 	},
 	{
 		_id: 4,
-		name: 'Scarlett',
+		name: 'Sophia',
 		animal: 'dog',
-		breed: 'Jack Russell Terrier',
+		breed: 'Shih tzu',
 		info:
-			'Scarlett is a happy, playful girl who will make you laugh and smile. She forms a bond quickly and will make a loyal companion and a wonderful family dog or a good companion for a single individual too since she likes to hang out and be with her human.',
-		img: 'pets-scarlet-min.png',
+			"Sophia here and I'm looking for my forever home to live out the best years of my life. I am full of energy. Everyday I'm learning new things, like how to walk on a leash, go potty outside, bark and play with toys and I still need some practice.",
+		img: 'pets-katrine-1-min.png',
 	},
 	{
 		_id: 5,
-		name: 'Katrine',
-		animal: 'cat',
-		breed: 'British Shorthair',
-		info:
-			'Katrine is a beautiful girl. She is as soft as the finest velvet with a thick lush fur. Will love you until the last breath she takes as long as you are the one. She is picky about her affection. She loves cuddles and to stretch into your hands for a deeper relaxations.',
-		img: 'pets-katrine-2-min.png',
-	},
-	{
-		_id: 6,
 		name: 'Timmy',
 		animal: 'cat',
 		breed: 'British Shorthair',
@@ -54,21 +46,30 @@ export default [
 		img: 'pets-timmy-min.png',
 	},
 	{
-		_id: 7,
-		name: 'Freddie',
-		animal: 'cat',
-		breed: 'British Shorthair',
-		info:
-			'Freddie is a little shy at first, but very sweet when he warms up. He likes playing with shoe strings and bottle caps. He is quick to learn the rhythms of his human’s daily life. Freddie has bounced around a lot in his life, and is looking to find his forever home.',
-		img: 'pets-katrine-min.png',
-	},
-	{
-		_id: 8,
+		_id: 6,
 		name: 'Charly',
 		animal: 'dog',
 		breed: 'Jack Russell Terrier',
 		info:
 			'This cute boy, Charly, is three years old and he likes adults and kids. He isn’t fond of many other dogs, so he might do best in a single dog home. Charly has lots of energy, and loves to run and play. We think a fenced yard would make him very happy.',
 		img: 'pets-charly-min.png',
+	},
+	{
+		_id: 7,
+		name: 'Scarlett',
+		animal: 'dog',
+		breed: 'Jack Russell Terrier',
+		info:
+			'Scarlett is a happy, playful girl who will make you laugh and smile. She forms a bond quickly and will make a loyal companion and a wonderful family dog or a good companion for a single individual too since she likes to hang out and be with her human.',
+		img: 'pets-scarlet-min.png',
+	},
+	{
+		_id: 8,
+		name: 'Freddie',
+		animal: 'cat',
+		breed: 'British Shorthair',
+		info:
+			'Freddie is a little shy at first, but very sweet when he warms up. He likes playing with shoe strings and bottle caps. He is quick to learn the rhythms of his human’s daily life. Freddie has bounced around a lot in his life, and is looking to find his forever home.',
+		img: 'pets-katrine-min.png',
 	},
 ];
