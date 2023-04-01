59509f3add52a8ef730f645cf690c045cf171bb5
lakdav <ladav2018@gmail.com>
2023-04-02 01:12:42 +0300

    feat: add response for friend section (md,sm)

--- shelter/js/index.js
+++ shelter/js/index.js
@@ -10,21 +10,32 @@ navigationToggleBtn.addEventListener('click', () => {
 	);
 	navigationMenu.classList.toggle('open');
 });
+//========================main page===========================
+const sliderContent = document.querySelector('.slider__content');
+if (sliderContent) {
+	let HTML_STRING = '';
+	for (let i = 0; i < data.length; i++) {
+		HTML_STRING += createContent(data[i]);
+	}
+	sliderContent.insertAdjacentHTML('afterbegin', HTML_STRING);
+}
 
-mediaSm.addEventListener('change', (e) => {});
-
+//========================pets page===========================
 const pets_friends_layout = document.getElementById('pets_layout');
 
 if (pets_friends_layout) {
 	let HTML_STRING = '';
 	for (let i = 0; i < data.length; i++) {
-		HTML_STRING += `<article class="card">
-   <div class="card__image">
-	   <img src="./assets/pets/${data[i].img}" alt="${data[i].name}">
-   </div>
-   <h2 class="card__name">${data[i].name}</h2>
-   <button id="card-modal" class="card__btn btn outlined">Learn more</button>
-   </article>`;
+		HTML_STRING += createContent(data[i]);
 	}
 	pets_friends_layout.insertAdjacentHTML('afterbegin', HTML_STRING);
+}
+function createContent(data) {
+	return `<article class="card">
+	<div class="card__image">
+		<img src="./assets/pets/${data.img}" alt="${data.name}">
+	</div>
+	<h2 class="card__name">${data.name}</h2>
+	<button id="card-modal" class="card__btn btn outlined">Learn more</button>
+	</article>`;
 }
